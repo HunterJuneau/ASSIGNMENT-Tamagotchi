2ca530e761aafa2db154c9ab92ad69a10b8c01b3
HunterJuneau <huntlee17@gmail.com>
2021-03-05 15:33:43 -0600

    Created Fight Component

--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -1,11 +1,13 @@
 import startEat from './components/eat';
 import startPlay from './components/play';
+import startFight from './components/fight';
 
 import '../styles/main.scss';
 
 const init = () => {
   startEat();
   startPlay();
+  startFight();
 };
 
 init();
